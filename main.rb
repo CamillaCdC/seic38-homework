886168e25686185ff55896ae814aeb883379ddbf
Camilla Champion de Crespigny <cjcdc@icloud.com>
2020-09-13 16:37:50 +1000

    created student folders

--- main.rb
+++ main.rb
@@ -5,4 +5,13 @@ names = file.read
 
 names_arr = names.split("\n")
 
-names_arr.each { |name| Dir.mkdir(name) }
+# names_arr.each do |name| 
+#     Dir.mkdir(name) 
+#     File.new(".txt", "w")
+# end
+
+names_arr.each do |name|
+    Dir.chdir(name)
+    File.new("empty.txt", "w")
+    Dir.chdir("..")
+end
