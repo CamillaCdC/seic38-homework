0764108b9e8e734cd27f793f470d91d285663275
Michelle <mjane668899@gmail.com>
2020-09-17 02:14:36 +0800

    Homework_for_Day_3

---
+++ Michelle Teoh/week_01/array_functions/bonus_hangman.js
@@ -0,0 +1,4 @@
+// Bonus: Make it like Hangman:
+// Keep track of all the guessed letters (right and wrong) and only let the user guess a letter once. If they guess a letter twice, do nothing.
+// Keep track of the state of the hangman as a number (starting at 0), and subtract or add to that number every time they make a wrong guess.
+// Once the number reaches 6 (a reasonable number of body parts for a hangman), inform the user that they lost and show a hangman on the log.
---
+++ Michelle Teoh/week_01/array_functions/bonus_wheel_of_fortune.js
@@ -0,0 +1,4 @@
+// Bonus: Make it more like Wheel of Fortune:
+// Start with a reward amount of $0
+// Every time a letter is guessed, generate a random amount and reward the user if they found a letter (multiplying the reward if multiple letters found), otherwise subtract from their reward.
+// When they guess the word, log their final reward amount.
