2492c8838353ace42b00699971e54a1f8c1bde9e
Jay Overall <jay@192-168-1-110.tpgi.com.au>
2020-09-16 10:29:44 +1000

    HOMEWORK BABBY

---
+++ Jasper Overall/JS Homework 01/homework.js
@@ -0,0 +1,60 @@
+console.log('Is this thing on?');
+//CACULATOR PART 1
+
+//Solution
+
+function squareNumber(num) {
+  var squaredNumber = num * num;
+  consle.log('The result of squaring the number ' + num + ' is ' + squaredNumber);
+  return squaredNumber;
+}
+
+squareNumber(3);
+
+function halfof(num); {
+var half = num / 2;
+console.log('half of ' + num + ' is ' + half);
+}
+
+//
+const squareNumber = function (num){
+var squaredNumber = num * num;
+consle.log('The result of squaring the number ' + num + ' is ' + squaredNumber);
+return squaredNumber;
+}
+
+//halfNumber
+
+halfOf(5);
+
+function percentOf(num1, num2) {
+  var percent = (num1/num2)* 100;
+  console.log(num1 + ' is ' + percent + '% of ' +num2);
+  return percent;
+}
+
+//Percentof
+
+percentOf(5, 10);
+
+function areaOfCircle(radius) {
+  car area =Matth.PI * squareNumber(radius);
+  console.log('The area of circle with radius ' + radius + ' is ' + area);
+  return area;
+}
+//areaofCirle
+
+function doCrazyStuff(num) {
+  var half =halfof(num);
+  var squared = squareNumber(half);
+  var area = areaofCircle(squared);
+  var result = percentof(squared, area);
+}
+
+//DoCrazyStuff(5);
+
+}
+}
+
+
+//CACULATOR PART 2
