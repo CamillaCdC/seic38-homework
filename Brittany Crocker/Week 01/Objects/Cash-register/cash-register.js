5f29478a2d15566e6caa3060ad64d6984fd6bc8b
Brittany Crocker <brittany.crocker@outlook.com>
2020-09-17 16:55:48 +1000

    Geometry problem sets complete, Cash-register in progress

---
+++ Brittany Crocker/Week 01/Objects/Cash-register/cash-register.js
@@ -0,0 +1,29 @@
+// # The Cash Register
+//
+// Write a function called cashRegister that takes a shopping cart object. The object contains item names and prices (itemName: itemPrice). The function should return the total price of the shopping cart.
+// Example
+//
+// ```
+// // Input
+const cartForParty = {
+  banana: "1.25",
+  handkerchief: ".99",
+  Tshirt: "25.01",
+  apple: "0.60",
+  nalgene: "10.34",
+  proteinShake: "22.36"
+};
+
+const cashRegister = function(itemName) {
+  for(i = 1; i < Object.keys(itemName).length; i++) {
+    console.log(i);
+  };
+};
+cashRegister(cartForParty)
+
+
+
+//
+// // Output
+// cashRegister(cartForParty)); // 60.55
+// ```
