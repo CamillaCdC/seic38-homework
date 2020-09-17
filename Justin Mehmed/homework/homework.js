b8be6b6ee760cfa1398dbc43dc9be0c00312850a
justinmehmed <justin.mehmed@yahoo.com>
2020-09-17 10:31:01 +1000

    home work

---
+++ Justin Mehmed/homework/homework.js
@@ -0,0 +1,36 @@
+// question 1
+//Write a function called squareNumber that will take one argument (a number),
+//square that number, and return the result.
+//It should also log a string like "The result of squaring the number 3 is 9."
+
+const fixStart = function(string){
+  let firstletter = string.chaArt(0);
+}
+  const
+}
+
+
+
+
+
+
+//Write a function called halfNumber that will take one argument (a number), divide it by 2, and return the result.
+//It should also log a string like "Half of 5 is 2.5.".
+const  halfNumber = function (num1) {
+
+const message = 'Half of ${ number} is ${ divided}');
+const divide = number / 2;
+console.log(message);
+}
+halfNumber(4);
+
+
+
+
+//Question 3
+
+const drEvil = function (dollars) {
+  let num = dollars
+  const evil = 1000000
+  if (num == evil)
+}
