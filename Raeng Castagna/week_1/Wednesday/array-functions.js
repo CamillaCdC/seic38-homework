b47beb2819aef6749ca67e7ae492dd7d90285928
Raeng Castagna <raengc@yahoo.com>
2020-09-17 10:52:56 +1000

    dont worry about this change

--- Raeng Castagna/week_1/Wednesday/array-functions.js
+++ Raeng Castagna/week_1/Wednesday/array-functions.js
@@ -31,7 +31,7 @@ console.log(`The largest number of three is ${maxOfThree(90, 3, 50)}`);
 // 3. Write a function that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
 
 const findVowel = function (vowel) {
-  if ( vowel.match(/[aeiou]/gi)) {
+  if ( vowel.match(/[aeiou]/gi) ) {
     return true;
   } else {
     return false;
