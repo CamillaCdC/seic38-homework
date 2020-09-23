d9569004cb4d457b0f60e93b97b7e6002edf2b68
Farid118 <faridmasjedi118@gmail.com>
2020-09-24 08:30:03 +1000

    HomeWork no3 - Week 02 - CatWalk

--- Farid Masjedi/week02/HomeWork3/CatWalk/js/catjs.js
+++ Farid Masjedi/week02/HomeWork3/CatWalk/js/catjs.js
@@ -1,97 +1,98 @@
-
-document.getElementById('cat-can-walk').addEventListener('click',function(){
-  const image = document.querySelector('img');
-  image.style.left = '0px';
-  const imageWidth = 300;
-  const pageWidth = window.innerWidth;
-  const stopWalkingWidth = pageWidth-imageWidth;
-
-  const catWalk = function () {
-
-    let walkingToRight = parseInt(image.style.left);
-    walkingToRight += 10;
-    image.style.left = `${walkingToRight}px`;
-    if (walkingToRight > stopWalkingWidth){
-      clearInterval(stop);
-    }
-  }
-  let stop = setInterval(catWalk,50);
-});
-
-
-
-document.getElementById('cat-dumb-walk').addEventListener('click', function(){
-  const image = document.querySelector('img');
-  image.style.left = '0px';
-
-  const imageWidth = 300;
-  const pageWidth = window.innerWidth;
-  const stopWalkingWidth = pageWidth-imageWidth;
-  let toIterateIndex = 0;
-
-  const catWalk = function () {
-
-    let walkingToRight = parseInt(image.style.left);
-
-    if (walkingToRight<stopWalkingWidth && toIterateIndex === 0){
-      walkingToRight += 10;
-      image.style.left = `${walkingToRight}px`;
-    }else{
-      toIterateIndex =1;
-    }
-    if (walkingToRight>=0 && toIterateIndex === 1){
-
-      walkingToRight -=10;
-      image.style.left = `${walkingToRight}px`;
-    }else{
-      toIterateIndex = 0;
-    }
-
-  }
-  setInterval(catWalk,50);
-
-})
-
-document.getElementById('cat-walk').addEventListener('click', function (){
-  const image = document.querySelector('img');
-  const dancingImg = document.querySelector('.dancing-cat');
-
-  image.style.left = '0px';
-  const imageWidth = 300;
-  const pageWidth = window.innerWidth;
-  const stopWalkingWidth = pageWidth-imageWidth;
-  let toIterateIndex = 0;
-
-  const catWalk = function () {
-
-    let walkingToRight = parseInt(image.style.left);
-
-    if (walkingToRight<stopWalkingWidth && toIterateIndex === 0){
-      image.style['transform'] = 'rotateY(0deg)';
-      walkingToRight += 10;
-      image.style.left = `${walkingToRight}px`;
-    }else{
-      toIterateIndex =1;
-
-      }
-    if (walkingToRight>=0 && toIterateIndex === 1){
-      image.style['transform'] = 'rotateY(180deg)';
-      walkingToRight -=10;
-      image.style.left = `${walkingToRight}px`;
-    }else{
-      toIterateIndex = 0;
-
-    };
-
-  }
-  setInterval(catWalk,50);
-})
+//
+// document.getElementById('cat-can-walk').addEventListener('click',function(){
+//   const image = document.querySelector('img');
+//   image.style.left = '0px';
+//   const imageWidth = 300;
+//   const pageWidth = window.innerWidth;
+//   const stopWalkingWidth = pageWidth-imageWidth;
+//
+//   const catWalk = function () {
+//
+//     let walkingToRight = parseInt(image.style.left);
+//     walkingToRight += 10;
+//     image.style.left = `${walkingToRight}px`;
+//     if (walkingToRight > stopWalkingWidth){
+//       clearInterval(stop);
+//     }
+//   }
+//   let stop = setInterval(catWalk,50);
+// });
+//
+//
+//
+// document.getElementById('cat-dumb-walk').addEventListener('click', function(){
+//   const image = document.querySelector('img');
+//   image.style.left = '0px';
+//
+//   const imageWidth = 300;
+//   const pageWidth = window.innerWidth;
+//   const stopWalkingWidth = pageWidth-imageWidth;
+//   let toIterateIndex = 0;
+//
+//   const catWalk = function () {
+//
+//     let walkingToRight = parseInt(image.style.left);
+//
+//     if (walkingToRight<stopWalkingWidth && toIterateIndex === 0){
+//       walkingToRight += 10;
+//       image.style.left = `${walkingToRight}px`;
+//     }else{
+//       toIterateIndex =1;
+//     }
+//     if (walkingToRight>=0 && toIterateIndex === 1){
+//
+//       walkingToRight -=10;
+//       image.style.left = `${walkingToRight}px`;
+//     }else{
+//       toIterateIndex = 0;
+//     }
+//
+//   }
+//   setInterval(catWalk,50);
+//
+// })
+//
+// document.getElementById('cat-walk').addEventListener('click', function (){
+//   const image = document.querySelector('img');
+//   const dancingImg = document.querySelector('.dancing-cat');
+//
+//   image.style.left = '0px';
+//   const imageWidth = 300;
+//   const pageWidth = window.innerWidth;
+//   const stopWalkingWidth = pageWidth-imageWidth;
+//   let toIterateIndex = 0;
+//
+//   const catWalk = function () {
+//
+//     let walkingToRight = parseInt(image.style.left);
+//
+//     if (walkingToRight<stopWalkingWidth && toIterateIndex === 0){
+//       image.style['transform'] = 'rotateY(0deg)';
+//       walkingToRight += 10;
+//       image.style.left = `${walkingToRight}px`;
+//     }else{
+//       toIterateIndex =1;
+//
+//       }
+//     if (walkingToRight>=0 && toIterateIndex === 1){
+//       image.style['transform'] = 'rotateY(180deg)';
+//       walkingToRight -=10;
+//       image.style.left = `${walkingToRight}px`;
+//     }else{
+//       toIterateIndex = 0;
+//
+//     };
+//
+//   }
+//   setInterval(catWalk,50);
+// })
 
 document.getElementById('disco-time').addEventListener('click', function (){
   const image = document.querySelector('.walking');
   const dancingCat = document.createElement('img');
   dancingCat.src = "js/dancing.gif";
   dancingCat.className = 'dancing-cat';
+  dancingCat.style['visibility'] = 'hidden';
   document.body.appendChild(dancingCat);
   const dancingImg = document.querySelector('.dancing-cat');
   const hide = document.querySelector('div');
@@ -113,7 +114,7 @@ document.getElementById('disco-time').addEventListener('click', function (){
     let walkingToRight = parseInt(image.style.left);
 
     if (walkingToRight<stopWalkingWidth && toIterateIndex === 0){
-      dancingImg.style['opacity'] = 1;
+      dancingImg.style['visibility'] = 'visible';
       image.style['transform'] = 'rotateY(0deg)';
       if(walkingToRight > Math.floor(pageWidth/2)-148 && walkingToRight < pageWidth/2){
         if(dicoLightIndex === discoLight.length-1){
