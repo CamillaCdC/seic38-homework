a83f67d63fca27ae08637f90cc72a989b7d201a1
Bastien Durand <basilexpeditions@gmail.com>
2020-09-25 16:49:19 +1000

    Start of jquery homework

--- Bastien Durand/week_02/atm/js/atm.js
+++ Bastien Durand/week_02/atm/js/atm.js
@@ -3,7 +3,48 @@ $(document).ready(function () {
   const checkingBalance = [];
   const savingsBalance = [];
 
-  const checkingInputValue = $('#checking-amount').val();
-  console.log( checkingInputValue)
+$('#checking-amount').on('keyup', function () {
+
+  //Store #input when clicked on #deposit COMPLETED
+
+$('#checking-deposit').on('click', function () {
+  let $checkAmount = $('#checking-amount').val();
+  var intCheckAmount = parseInt($checkAmount)
+  intCheckAmount.next(intCheckAmount)
+  console.log(intCheckAmount)
+  console.log(checkAmount)
+  $('#checking-balance').text(checkAmount)
+$('#checking-withdraw').on('click', function () {
+  let newCheckAmount = $('#checking-amount').val()
+  $('#checking-balance').text(intCheckAmount - newCheckAmount)
+})
+})
+
+// $('#checking-withdraw').on('click', function () {
+//   const depAmount = $('#checking-balance').val();
+//   console.log(depAmount)
+//   $('#checking-amount').text(depAmount -= depAmount)
+// })
+
+
+  //push stored #input into #balance
+
+  //clear stored #balance when clicking withdraw
 
 })
+
+//Same thing for SAVINGS
+
+
+
+
+
+})
+// $('#savings-amount').on('keyup', function () {
+//
+//
+//   $('savings-deposit').on('click', function () {
+//     const inputSav = $('savings-amount').val();
+//     console.log(inputSav)
+//   })
+// })
