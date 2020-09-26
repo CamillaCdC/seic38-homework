caf48c7333ae0df8b493b13ba6f0a5732b625ca3
Brittany Crocker <brittany.crocker@outlook.com>
2020-09-26 14:53:28 +1000

    can withdraw, deposit and account background changes colour depending on balance

--- Brittany Crocker/Week 02/atm/atm/js/atm.js
+++ Brittany Crocker/Week 02/atm/atm/js/atm.js
@@ -3,67 +3,76 @@ $(document).ready(function () {
 let savingsBalance = 0;
 let checkingsBalance = 0;
 
+// * When the balance of the bank account is $0 the background of that bank account
+// should be red. It should be gray when there is money in the account.
+const changeColourSavings = function() {
+  if (savingsBalance === 0) {
+        $('#savings-balance').css("background-color", "red")
+  } else {
+      $('#savings-balance').css("background-color", "lightgray")
+
+  }
+}
+
+const changeColourCheckings = function() {
+  if (checkingsBalance === 0) {
+    $('#checking-balance').css("background-color", "red")
+  } else {
+    $('#checking-balance').css("background-color", "lightgray")
+  }
+}
 
 
-
-
-
-// * Keep track of the checking and savings balances somewhere
 // * Add functionality so that a user can deposit money into one of the bank accounts.
 
+// Deposit to checking
 const depositToChecking = function(e) {
-const checkingAmountInput =  $('#checking-amount').val()
 
+  const checkingAmountInput =  $('#checking-amount').val()
+  $('#checking-balance').text(checkingAmountInput)
 
-$('#checking-balance').text(checkingAmountInput)
+    checkingsBalance = parseInt($('#checking-balance').text()) + checkingsBalance
+    $('#checking-balance').text("$" + checkingsBalance)
 
-checkingsBalance = parseInt($('#checking-balance').text()) + checkingsBalance
-$('#checking-balance').text(checkingsBalance)
-
+  changeColourCheckings()
 }
 
 $('#checking-deposit').on('click', depositToChecking)
 
-
+// Deposit to savings
 const depositToSavings = function(e) {
 
+  const savingsAmountInput =  $('#savings-amount').val()
+  $('#savings-balance').text(savingsAmountInput)
 
-const savingsAmountInput =  $('#savings-amount').val()
-$('#savings-balance').text(savingsAmountInput)
+      savingsBalance =  parseInt($('#savings-balance').text()) + savingsBalance
+      $('#savings-balance').text("$" + savingsBalance)
 
-
-    savingsBalance =  parseInt($('#savings-balance').text()) + savingsBalance
-    console.log(savingsBalance)
-    $('#savings-balance').text(savingsBalance)
+    changeColourSavings()
 }
-
-
-
-
 $('#savings-deposit').on('click', depositToSavings)
 
 
-// * Make sure you are updating the display and manipulating the HTML of the page
-// so a user can see the change.
 // * Add functionality so that a user can withdraw money from one of the bank accounts.
 
- // up to here -------------
+// Withdraw from checking
 const withdrawFromChecking = function(e) {
+
   const checkingAmountInput =  parseInt($('#checking-amount').val())
 
-  if (parseInt($('#checking-amount').val()) > checkingsBalance) {
-    checkingsBalance === checkingsBalance
+    if (parseInt($('#checking-amount').val()) > checkingsBalance) {
+      checkingsBalance === checkingsBalance
 
-  } else {
-    checkingsBalance = checkingsBalance - checkingAmountInput
-
-    $('#checking-balance').text(checkingsBalance)
+    } else {
+      checkingsBalance = checkingsBalance - checkingAmountInput
+      $('#checking-balance').text("$" + checkingsBalance)
   }
-}
 
+  changeColourCheckings()
+}
 $('#checking-withdraw').on('click', withdrawFromChecking)
 
-
+// Withdraw from savings
 const withdrawFromSavings = function(e) {
   const savingsAmountInput = parseInt($('#savings-amount').val())
 
@@ -72,20 +81,16 @@ const withdrawFromSavings = function(e) {
 
   } else {
     savingsBalance = savingsBalance - savingsAmountInput
-
-    $('#savings-balance').text(savingsBalance)
+    $('#savings-balance').text("$" + savingsBalance)
 
   }
-}
 
+  changeColourSavings()
+}
 $('#savings-withdraw').on('click', withdrawFromSavings)
 
-// * Make sure you are updating the display and manipulating the HTML of the page
-// so a user can see the change.
-// * Make sure the balance in an account can't go negative. If a user tries to
-// withdraw more money than exists in the account, ignore the transaction.
-// * When the balance of the bank account is $0 the background of that bank account
-// should be red. It should be gray when there is money in the account. // call a class in css
+
+
 // * What happens when the user wants to withdraw more money from the checking
 // account than is in the account? These accounts have overdraft protection, so if
 // a withdrawal can be covered by the balances in both accounts, take the checking
