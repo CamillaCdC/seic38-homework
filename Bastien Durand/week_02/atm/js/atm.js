d6c8b0433952165192bde5dd707267cb559fa33f
Bastien Durand <basilexpeditions@gmail.com>
2020-09-26 14:44:35 +1000

    atm homework, jquery and javascript

--- Bastien Durand/week_02/atm/js/atm.js
+++ Bastien Durand/week_02/atm/js/atm.js
@@ -1,50 +1,91 @@
 $(document).ready(function () {
 
-  const checkingBalance = [];
-  const savingsBalance = [];
+  let checkingBalance = 0
+  let savingsBalance = 0
 
-$('#checking-amount').on('keyup', function () {
+const colorCheckerChecking = function () {
+  if(checkingBalance === 0) {
+    $('#checking-balance').css('background-color', 'red')
+  } else {
+    $('#checking-balance').css('background-color', 'green')
+  }
+};
 
-  //Store #input when clicked on #deposit COMPLETED
+const colorCheckerSavings = function () {
+  if(savingsBalance === 0) {
+    $('#savings-balance').css('background-color', 'red')
+  } else {
+    $('#savings-balance').css('background-color', 'green')
+  }
+};
 
+  if(checkingBalance === 0) {
+    $('#checking-balance').css('background-color', 'red')
+  }
+
+  if(savingsBalance === 0) {
+    $('#savings-balance').css('background-color', 'red')
+  }
+
+
+//CHECKING
+//DEPOSITING
 $('#checking-deposit').on('click', function () {
-  let $checkAmount = $('#checking-amount').val();
-  var intCheckAmount = parseInt($checkAmount)
-  intCheckAmount.next(intCheckAmount)
-  console.log(intCheckAmount)
-  console.log(checkAmount)
-  $('#checking-balance').text(checkAmount)
-$('#checking-withdraw').on('click', function () {
-  let newCheckAmount = $('#checking-amount').val()
-  $('#checking-balance').text(intCheckAmount - newCheckAmount)
-})
+  let depositAmount = $('#checking-amount').val()
+    $('#checking-balance').text(depositAmount)
+    checkingBalance =  parseInt($('#checking-balance').text()) + checkingBalance
+    $('#checking-balance').text(checkingBalance)
+
+
+    colorCheckerChecking();
 })
 
-// $('#checking-withdraw').on('click', function () {
-//   const depAmount = $('#checking-balance').val();
-//   console.log(depAmount)
-//   $('#checking-amount').text(depAmount -= depAmount)
-// })
+//CHECKING
+//WITHDRAWING
+$('#checking-withdraw').on('click', function (){
+  let withdrawAmount = parseInt($('#checking-amount').val())
 
+  if(withdrawAmount > checkingBalance) {
+    checkingBalance === checkingBalance
+    console.log('Cannot withdraw amount')
+  } else {
+    $('#checking-balance').text(withdrawAmount)
+    checkingBalance =  checkingBalance - parseInt($('#checking-balance').text())
+    $('#checking-balance').text(checkingBalance)
+  }
 
-  //push stored #input into #balance
-
-  //clear stored #balance when clicking withdraw
-
-})
-
-//Same thing for SAVINGS
-
-
-
+  colorCheckerChecking();
 
 
 })
-// $('#savings-amount').on('keyup', function () {
-//
-//
-//   $('savings-deposit').on('click', function () {
-//     const inputSav = $('savings-amount').val();
-//     console.log(inputSav)
-//   })
-// })
+
+//SAVINGS
+//WITHDRAWING
+$('#savings-deposit').on('click', function () {
+  let depositAmount = $('#savings-amount').val()
+    $('#savings-balance').text(depositAmount)
+    savingsBalance =  parseInt($('#savings-balance').text()) + savingsBalance
+    $('#savings-balance').text(savingsBalance)
+
+    colorCheckerSavings();
+})
+
+//Savings
+//DEPOSITING
+$('#savings-withdraw').on('click', function (){
+  let withdrawAmount = $('#savings-amount').val()
+
+  if(withdrawAmount > savingsBalance) {
+    savingsBalance === savingsBalance
+    console.log('Cannot withdraw amount')
+  } else {
+    $('#savings-balance').text(withdrawAmount)
+    savingsBalance =  savingsBalance - parseInt($('#savings-balance').text())
+    $('#savings-balance').text(savingsBalance)
+  }
+
+  colorCheckerSavings();
+
+})
+
+})
