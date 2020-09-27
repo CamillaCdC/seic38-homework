9b1f5111d2dbb7674051f8ce606fdfc108e4c3ec
Guillaume Canneton <g.canneton@gmail.com>
2020-09-28 08:47:56 +1000

    some minor improvements on ATM project

--- Guillaume Canneton/week02/atm/js/atm.js
+++ Guillaume Canneton/week02/atm/js/atm.js
@@ -2,55 +2,72 @@ let checkBalance = 0;
 let savBalance = 0;
 // Checking account part
 //first button - deposit on checking account
+$( document ).ready(function() {
 
 
 $('#checking-deposit').on('click', function(){
   let $checkAmount = $( '#checking-amount' ).val();
+  if ( isNaN(parseInt( $checkAmount )) ){
+    window.alert("Please enter a number");//this statement to avoid a bug with NaN value
+  } else {
   checkBalance += parseInt( $checkAmount );
   $( '#checking-balance' ).text( '$' + checkBalance );
   alertBalance();
+  }
 });
 //second button - withdraw on checking account
 $( '#checking-withdraw' ).on( 'click', function(){
   let $checkAmount = $( '#checking-amount' ).val();
-  if ( $checkAmount <= checkBalance ) {
-    checkBalance -= parseInt( $checkAmount );
-    $( '#checking-balance' ).text( '$' + checkBalance );
-    alertBalance();
-  } else if ( $checkAmount <= ( checkBalance + savBalance ) ){
-    savBalance = savBalance + ( checkBalance - $checkAmount );
-    $( '#savings-balance' ).text( '$' + savBalance );
-    checkBalance = 0;
-    $( '#checking-balance' ).text( '$' + checkBalance );
-    alertBalance();
+  if ( isNaN(parseInt( $checkAmount )) ){
+    window.alert("Please enter a number");//this statement to avoid a bug with NaN value
   } else {
-    console.log('error');
+    if ( $checkAmount <= checkBalance ) {
+      checkBalance -= parseInt( $checkAmount );
+      $( '#checking-balance' ).text( '$' + checkBalance );
+      alertBalance();
+    } else if ( $checkAmount <= ( checkBalance + savBalance ) ){
+      savBalance = savBalance + ( checkBalance - $checkAmount );
+      $( '#savings-balance' ).text( '$' + savBalance );
+      checkBalance = 0;
+      $( '#checking-balance' ).text( '$' + checkBalance );
+      alertBalance();
+    } else {
+      $( '#alert').css( 'visibility', 'visible')
+    }
   }
 });
 //Savings account part
 //first button - deposit on savings account
 $( '#savings-deposit' ).on( 'click', function(){
   let $savAmount = $( '#savings-amount' ).val();
+  if ( isNaN(parseInt( $savAmount )) ){
+    window.alert("Please enter a number");//this statement to avoid a bug with NaN value
+  } else {
   savBalance += parseInt( $savAmount );
   $( '#savings-balance' ).text( '$' + savBalance );
   alertBalance();
+  }
 });
 
 //second button - withdraw on savings account
 $( '#savings-withdraw' ).on( 'click', function(){
   let $savAmount = $( '#savings-amount' ).val();
-  if ( $savAmount <= savBalance ) {
-    savBalance -= parseInt( $savAmount );
-    $( '#savings-balance' ).text( '$' + savBalance );
-    alertBalance();
-  } else if ( $savAmount <= ( checkBalance + savBalance )){
-    checkBalance = checkBalance + ( savBalance - $savAmount );
-    $( '#checking-balance' ).text( '$' + checkBalance );
-    savBalance = 0;
-    $( '#savings-balance' ).text( '$' + savBalance );
-    alertBalance();
+  if ( isNaN(parseInt( $savAmount )) ){
+    window.alert("Please enter a number");//this statement to avoid a bug with NaN value
   } else {
-    console.log('error');
+    if ( $savAmount <= savBalance ) {
+      savBalance -= parseInt( $savAmount );
+      $( '#savings-balance' ).text( '$' + savBalance );
+      alertBalance();
+    } else if ( $savAmount <= ( checkBalance + savBalance )){
+      checkBalance = checkBalance + ( savBalance - $savAmount );
+      $( '#checking-balance' ).text( '$' + checkBalance );
+      savBalance = 0;
+      $( '#savings-balance' ).text( '$' + savBalance );
+      alertBalance();
+    } else {
+      $( '#alert').css( 'visibility', 'visible')
+    }
   }
 });
 //This function set the background color of balance for checking and savings
@@ -65,5 +82,10 @@ const alertBalance = function(){
   } else {
     $( '#savings-balance' ).removeClass( 'zero' )
   };
+  if ( checkBalance > 0 || savBalance > 0 ){
+    $( '#alert').css( 'visibility', 'hidden')
+  }
 }
 alertBalance(); //run the function to set the background at the first load of the page
+
+});
