71db6ca71debd559c51b07bda79330137af91136
Brittany Crocker <brittany.crocker@outlook.com>
2020-10-07 10:21:46 +1100

    HW completed

--- Brittany Crocker/Week 04/calculator.rb
+++ Brittany Crocker/Week 04/calculator.rb
@@ -1,3 +1,25 @@
+# # Calculator
+#
+# ### Explanation
+# - You will be building a calculator.  A calculator can perform multiple arithmetic operations.  Your function should allow the user to choose which operation is expected, enter in the values to perform the operation on, and ultimately view the result.
+#
+# ### Specification:
+# - A user should be given a menu of operations
+# - A user should be able to choose from the menu
+# - A user should be able to enter numbers to perform the operation on
+# - A user should be shown the result
+# - This process should continue until the user selects a quit option from the menu
+#
+# #### Phase 1
+# - Calculator functionality
+# - Calculator should be able to do basic arithmetic (+,-, *, /)
+#
+# #### Phase 2
+# - Advanced Calculator functionality
+# - Calculator should be able to do basic arithmetic (exponents, square roots)
+#
+
+
 # Menu
 def show_menu
 puts "Calculator" # You can use .center here to make it look nice.
@@ -96,3 +118,6 @@ end
 end
 
 puts "Thanks for using the crappy calculator!"
+
+# ## BMI Calculator
+# Calculate the body mass index (BMI) for an individual, given their height and weight
