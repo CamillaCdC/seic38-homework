3d52c5d822e902ee09e740f3b2144aa20933fdfc
Carla Rudder <CarlaRudder@WilliamRudder.local>
2020-10-12 16:18:50 +1100

    SQL RUBY task

---
+++ William Rudder/week4/SQLRUBY/beaches/beaches.sql
@@ -0,0 +1,11 @@
+CREATE TABLE beaches (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  beach TEXT,
+  country TEXT,
+  image TEXT
+);
+
+INSERT INTO beaches (beach, country) VALUES ('Bondi', 'Australia');
+
+-- integer     TEXT        TEXT            TEXT
+-- id          name        family          image (url)
