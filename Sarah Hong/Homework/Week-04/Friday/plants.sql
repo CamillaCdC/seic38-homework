b482c97dd25a03266443dd86a8b108580bffcec4
sarahhae <helloitssarahhae@gmail.com>
2020-10-14 11:20:20 +1300

    Week 4 homework

---
+++ Sarah Hong/Homework/Week-04/Friday/plants.sql
@@ -0,0 +1,11 @@
+-- CREATE TABLE plants (
+--   id INTEGER PRIMARY KEY AUTOINCREMENT,
+--   name TEXT,
+--   family TEXT,
+--   image TEXT
+-- );
+
+-- INSERT INTO plants (name, family) VALUES ('Deliciosa', 'Monstera');
+-- INSERT INTO plants (name, family) VALUES ('Variegata', 'Monstera');
+-- INSERT INTO plants (name, family) VALUES ('Adansonii', 'Monstera');
+-- INSERT INTO plants (name, family) VALUES ('Rhaphidophora Tetrasperma', 'Monstera');
