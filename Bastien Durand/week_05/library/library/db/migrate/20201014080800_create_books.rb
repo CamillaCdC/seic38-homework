32840f40129ed182bde36d456ca8c8d3d1b21313
Bastien Durand <basilexpeditions@gmail.com>
2020-10-14 20:18:26 +1100

    Library of books, Ruby on Rails practice

---
+++ Bastien Durand/week_05/library/library/db/migrate/20201014080800_create_books.rb
@@ -0,0 +1,14 @@
+class CreateBooks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :books do |t|
+
+      t.text :name
+      t.text :author
+      t.text :year
+      t.text :image
+
+
+      t.timestamps
+    end
+  end
+end
