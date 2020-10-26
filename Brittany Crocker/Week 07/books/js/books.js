7c928a74cbcc33b8422061eede2d0ced83c14c34
Brittany Crocker <brittany.crocker@outlook.com>
2020-10-26 16:56:42 +1100

    added a try catch - error handling - for bookprice

--- Brittany Crocker/Week 07/books/js/books.js
+++ Brittany Crocker/Week 07/books/js/books.js
@@ -19,17 +19,30 @@ let bookName = document.getElementById('book_name').value
       h3.innerHTML = title;
       document.body.appendChild( h3 );
 
-      const img = document.createElement('img')
-        const thumbnail = data["items"][0]["volumeInfo"]["imageLinks"]["thumbnail"]
-        img.src = thumbnail
-        document.body.appendChild(img)
+    const img = document.createElement('img')
+      const thumbnail = data["items"][0]["volumeInfo"]["imageLinks"]["thumbnail"]
+      img.src = thumbnail
+      document.body.appendChild(img)
 
     const h5 = document.createElement('h5');
       const author = data["items"][0]["volumeInfo"]["authors"].join(" ")
       console.log(author)
       h5.innerHTML = `By ${author}`
       document.body.appendChild(h5)
+      try {
+    const bookPrice_el = document.createElement('h6')
+    const bookPrice = data["items"][0]["saleInfo"]["listPrice"]["amount"]
+    bookPrice_el.innerHTML = bookPrice
+    document.body.appendChild(bookPrice_el)
 
+    const currency = document.createElement('h6');
+      const bookPrice_currency = data["items"][0]["saleInfo"]["listPrice"]["currencyCode"]
+      currency.innerHTML = bookPrice_currency
+      document.body.appendChild(currency)
+    }
+    catch(err) {
+      bookPrice = "No price"
+    }
     const p = document.createElement('p')
       const blurb = data["items"][0]["volumeInfo"]["description"]
       p.innerHTML = blurb
