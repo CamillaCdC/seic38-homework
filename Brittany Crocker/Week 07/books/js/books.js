90960dff6fc1877ebe59874db4ec86299056c1c6
Brittany Crocker <brittany.crocker@outlook.com>
2020-10-27 10:23:42 +1100

    HW finished

--- Brittany Crocker/Week 07/books/js/books.js
+++ Brittany Crocker/Week 07/books/js/books.js
@@ -14,33 +14,33 @@ let bookName = document.getElementById('book_name').value
     const data = JSON.parse(xhr.responseText);
 
     const h3 = document.createElement('h3');
-      const title = data["items"][0]["volumeInfo"]["title"]
+      const title = data["items"][0]["volumeInfo"]["title"];
       h3.innerHTML = title;
       document.body.appendChild( h3 );
 
-    const img = document.createElement('img')
-      const thumbnail = data["items"][0]["volumeInfo"]["imageLinks"]["thumbnail"]
-      img.src = thumbnail
-      document.body.appendChild(img)
+    const img = document.createElement('img');
+      const thumbnail = data["items"][0]["volumeInfo"]["imageLinks"]["thumbnail"];
+      img.src = thumbnail;
+      document.body.appendChild(img);
 
     const h5 = document.createElement('h5');
-      const author = data["items"][0]["volumeInfo"]["authors"].join(" ")
-      console.log(author)
-      h5.innerHTML = `By ${author}`
-      document.body.appendChild(h5)
+      const author = data["items"][0]["volumeInfo"]["authors"].join(" ");
+      console.log(author);
+      h5.innerHTML = `By ${author}`;
+      document.body.appendChild(h5);
 
       try {
-    const bookPrice_el = document.createElement('h6')
-      const bookPrice = data["items"][0]["saleInfo"]["listPrice"]["amount"]
-      bookPrice_el.innerHTML = bookPrice
-      document.body.appendChild(bookPrice_el)
+    const bookPrice_el = document.createElement('h6');
+      const bookPrice = data["items"][0]["saleInfo"]["listPrice"]["amount"];
+      bookPrice_el.innerHTML = bookPrice;
+      document.body.appendChild(bookPrice_el);
     const currency = document.createElement('h6');
-      const bookPrice_currency = data["items"][0]["saleInfo"]["listPrice"]["currencyCode"]
-      currency.innerHTML = bookPrice_currency
-      document.body.appendChild(currency)
+      const bookPrice_currency = data["items"][0]["saleInfo"]["listPrice"]["currencyCode"];
+      currency.innerHTML = bookPrice_currency;
+      document.body.appendChild(currency);
     }
     catch(err) {
-      bookPrice = "No price"
+      let bookPrice = "No price"
     }
 
     const p = document.createElement('p')
