ce2bc2f8b1eb5864089a6d45b3273bb53e5047df
Brittany Crocker <brittany.crocker@outlook.com>
2020-10-26 17:31:56 +1100

    code indentation cleaned

--- Brittany Crocker/Week 07/books/js/books.js
+++ Brittany Crocker/Week 07/books/js/books.js
@@ -1,4 +1,4 @@
-const fetchBook = function (e) {
+const fetchBook = function () {
 let bookName = document.getElementById('book_name').value
 
   const xhr = new XMLHttpRequest();
@@ -15,7 +15,6 @@ let bookName = document.getElementById('book_name').value
 
     const h3 = document.createElement('h3');
       const title = data["items"][0]["volumeInfo"]["title"]
-      console.log(title);
       h3.innerHTML = title;
       document.body.appendChild( h3 );
 
@@ -29,12 +28,12 @@ let bookName = document.getElementById('book_name').value
       console.log(author)
       h5.innerHTML = `By ${author}`
       document.body.appendChild(h5)
+
       try {
     const bookPrice_el = document.createElement('h6')
-    const bookPrice = data["items"][0]["saleInfo"]["listPrice"]["amount"]
-    bookPrice_el.innerHTML = bookPrice
-    document.body.appendChild(bookPrice_el)
-
+      const bookPrice = data["items"][0]["saleInfo"]["listPrice"]["amount"]
+      bookPrice_el.innerHTML = bookPrice
+      document.body.appendChild(bookPrice_el)
     const currency = document.createElement('h6');
       const bookPrice_currency = data["items"][0]["saleInfo"]["listPrice"]["currencyCode"]
       currency.innerHTML = bookPrice_currency
@@ -43,6 +42,7 @@ let bookName = document.getElementById('book_name').value
     catch(err) {
       bookPrice = "No price"
     }
+
     const p = document.createElement('p')
       const blurb = data["items"][0]["volumeInfo"]["description"]
       p.innerHTML = blurb
@@ -53,6 +53,5 @@ let bookName = document.getElementById('book_name').value
 
   };
 };
-
 
 document.getElementById('fetch').addEventListener('click', fetchBook)
