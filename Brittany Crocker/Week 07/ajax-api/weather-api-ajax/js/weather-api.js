6dc3d346d612b012137d1a9a6e1c25badffaccfd
Brittany Crocker <brittany.crocker@outlook.com>
2020-10-27 13:07:44 +1100

    removed weather api

--- Brittany Crocker/Week 07/ajax-api/weather-api-ajax/js/weather-api.js
+++ Brittany Crocker/Week 07/ajax-api/weather-api-ajax/js/weather-api.js
@@ -1,7 +1,7 @@
 const fetchFact = function () {
 
   let searchTerm = $('#search').val()
-  $.ajax(`api.openweathermap.org/data/2.5/weather?q={London}&appid={793ac2d1685cb9fbb4b94011f77ae0f3
+  $.ajax(`
   }`).done(function (info) {
     $('#submit').after(`<p>${info.weather}</p>`)
   });
