54c9da7f1a21f1e479f90c1fa33e09e57049719c
Brittany Crocker <brittany.crocker@outlook.com>
2020-10-27 13:05:32 +1100

    trialling weather api

--- Brittany Crocker/Week 07/ajax-api/weather-api-ajax/js/weather-api.js
+++ Brittany Crocker/Week 07/ajax-api/weather-api-ajax/js/weather-api.js
@@ -3,7 +3,7 @@ const fetchFact = function () {
   let searchTerm = $('#search').val()
   $.ajax(`api.openweathermap.org/data/2.5/weather?q={London}&appid={793ac2d1685cb9fbb4b94011f77ae0f3
   }`).done(function (info) {
-    $('#submit').after(`<p>${info.joke}</p>`)
+    $('#submit').after(`<p>${info.weather}</p>`)
   });
 
 };
