14ee91ff3560f82371a5d27d42ee971e8390f436
YOUR_FULL NAME_HERE <your@email.here.com>
2020-10-27 13:07:08 +1100

    final

--- Giti Mafakheri/week-7/google_books/js/ajax-intro.js
+++ Giti Mafakheri/week-7/google_books/js/ajax-intro.js
@@ -1,22 +1,22 @@
 const fetchFact = function(){
+
+  const title = document.getElementById('title').value;
   const xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://www.googleapis.com/books/v1/volumes?q=title?json');
+  const mainUrl =`https://www.googleapis.com/books/v1/volumes?q=title:${title}`;
+  console.log(mainUrl);
+  xhr.open('GET', mainUrl);
   xhr.send();
 
 xhr.onreadystatechange =function(){
 if (xhr.readyState !== 4) return;
-
-
 let x = document.createElement("IMG");
-console.log(x);
 let data = JSON.parse (xhr.responseText);
 let url = data.items[0].volumeInfo.imageLinks.thumbnail;
-console.log("url is");
-console.log(url);
 x.src = url;
 document.body.appendChild(x);
 
 };
 
 };
+
 document.getElementById('fetch').addEventListener('click', fetchFact);
