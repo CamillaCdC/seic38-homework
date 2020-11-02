0428755f54aa577baa738ee96378ff4b66f98e3d
Ryan Bullough <rjbullough@gmail.com>
2020-11-02 21:05:42 +1100

    Gosh what a pain

--- Ryan Bullough/week08/monday-react-api/wger-app/src/components/App.js
+++ Ryan Bullough/week08/monday-react-api/wger-app/src/components/App.js
@@ -1,8 +1,19 @@
+import React, { useState } from "react";
+import SearchForm from "./SearchForm";
+import ExerciseList from "./ExerciseList";
+
 function App() {
+  const [exercises, setExercises] = useState([]);
+
+  const addExercises = (exercise) => {
+    setExercises(exercise);
+  };
+
   return (
     <div className="App">
       <h1>Exercise Browser</h1>
-      <SearchForm />
+      <SearchForm onSubmit={addExercises} />
+      <ExerciseList selection={exercises} />
     </div>
   );
 }
